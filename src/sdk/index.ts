5da2a12de34f2c46966ad700b3ee86baeeb3d1dd
ef-code <111162892+EF-Code@users.noreply.github.com>
2026-08-05 14:59:15 +0100

    feat(sdk): add main SDK unified index export

---
+++ src/sdk/index.ts
@@ -0,0 +1,6 @@
+export * from './types.js';
+export * from './crypto-utils.js';
+export * from './nullifier.js';
+export * from './initdata-parser.js';
+export * from './proof-generator.js';
+export * from './proof-verifier.js';
